468afe69386f0378cc684214a8a93f28655a30d5
Kooreis <kooreis.official@gmail.com>
2024-03-25 20:18:36 -0500

    This is the main function that prompts the user to input two strings. It stores the inputs in str1 and str2.

--- solution.py
+++ solution.py
@@ -1,5 +1,3 @@
-def check_scramble(str1, str2):
-    if sorted(str1) == sorted(str2):
-        return True
-    else:
-        return False
+def main():
+    str1 = input("Enter the first string: ")
+    str2 = input("Enter the second string: ")
